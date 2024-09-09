15a8ed8007814918ce1ebf4f0b98d3a98bc1e7a8
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-09-09 09:37:43 -0300

    update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
 import { MyJwtPayload } from "next-auth";
 
 export default function Navbar() {
   const { data: session } = useSession();
   const route = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Controla o estado do menu
   const decodedToken = session?.user?.access_token
     ? jwtDecode<MyJwtPayload>(session.user.access_token)
     : null;
@@ -17,19 +19,24 @@ export default function Navbar() {
     route.replace("/");
   }
 
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <nav className="bg-[#FF8225] border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <span className="self-center text-2xl font-semibold whitespace-nowrap">
-          Gerenciador de Taredas
+          Gerenciador de Tarefas
         </span>
 
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          onClick={toggleMenu}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 dark:text-white"
           aria-controls="navbar-default"
-          aria-expanded="false"
+          aria-expanded={isMenuOpen}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -41,14 +48,19 @@ export default function Navbar() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={`w-full md:block md:w-auto ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             <p className="font-semibold hover:text-white">
               Olá, {decodedToken?.name}
@@ -56,7 +68,7 @@ export default function Navbar() {
             <li>
               <button
                 onClick={logout}
-                className="font-semibold block py-2 px-3 rounded text-white md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0 "
+                className="font-semibold block py-2 px-3 rounded text-white md:hover:bg-transparent md:border-0 md:hover:text-white-700 md:p-0"
               >
                 Sair
               </button>
